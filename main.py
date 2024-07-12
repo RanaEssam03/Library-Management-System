2d11ebb67280a6b7dbb48f2fcfa952febe8d7d11
Rana Essam <95047435+RanaEssam03@users.noreply.github.com>
2024-07-13 01:19:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from services.book_services import app
 
 # This script runs the Flask application for book services.
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
