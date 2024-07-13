8fbab0f52862729e8eae82ef9c82fea85179457e
Rana Essam <95047435+RanaEssam03@users.noreply.github.com>
2024-07-13 22:15:53 +0300

    removing redundant folders

--- main.py
+++
@@ -1,6 +0,0 @@
-from services.book_services import app
-
-# This script runs the Flask application for book services.
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
- 
